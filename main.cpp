e060727ddf916a44ef7bbf874f7be76092fada5d
Maksim <122976125+Maxim12e@users.noreply.github.com>
2023-09-08 20:48:06 +0300

    New project

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    std::cout << "Maksim!" << std::endl;
+    std::cout << "Привет, как дела ?" << std::endl;
+
+    return 0;
+}
